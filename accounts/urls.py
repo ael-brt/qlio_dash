0f61aee828727d0d863918fcb6b9998c6824a80d
ael-brt <bourasseta@gmail.com>
2025-09-28 17:12:52 +0200

    Ajout du layout base.html + gestion utilisateurs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import signup
 
 urlpatterns = [
     path('login/',  LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),  # <--- ajout next_page
+    path('signup/', signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # connexion auto après inscription
+            return redirect("dashboard:index")
+    else:
+        form = SignUpForm()
+    return render(request, "registration/signup.html", {"form": form})
